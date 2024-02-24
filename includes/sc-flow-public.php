3016d15a651c92b18a10aa02fa802916424330e7
Mohamed Habbat <mohamedhabbat@icloud.com>
2024-02-24 14:13:50 +0100

    fix: SCFlowPluginSettings for connect wallet wagmi

--- includes/sc-flow-public.php
+++ includes/sc-flow-public.php
@@ -45,7 +45,6 @@ class SmartContract_Flow_Public
         // This will work on browsers that support newer Javascript syntax
         // Get the saved slugs option.
         $slugs_option = get_option(MC_ADMIN_SLUGS_FIELD);
-
         // Check if we have saved slugs.
         if (!empty($slugs_option)) {
             // Convert the comma-separated slugs into an array.
@@ -53,10 +52,15 @@ class SmartContract_Flow_Public
 
             // Check if the current page's slug matches any of the saved slugs.
             $current_slug = basename(get_permalink());
-
             if (in_array($current_slug, $slugs)) {
-
-                echo '<script type="module" src="' . plugin_dir_url(PLUGIN_ROOT_PATH) . 'assets/js/connect-wallet-wagmi.js"/>';
+                $sc_flow_plugin_settings = $this->get_sc_flow_plugin_settings();
+    
+                // Move the script enqueue and inline script inside the slug check
+                wp_enqueue_script('placeholder-for-inline-script', '', array('jquery'), true);
+                $inline_script = 'window.SCFlowPluginSettings = ' . json_encode($sc_flow_plugin_settings) . ';';
+                wp_add_inline_script('placeholder-for-inline-script', $inline_script, 'before');
+    
+                echo '<script type="module" src="' . plugin_dir_url(PLUGIN_ROOT_PATH) . 'assets/js/connect-wallet-wagmi.js"></script>';
             }
         }
     }
@@ -77,40 +81,11 @@ class SmartContract_Flow_Public
             $current_slug = basename(get_permalink());
 
             if (in_array($current_slug, $slugs)) {
+
+                $sc_flow_plugin_settings = $this->get_sc_flow_plugin_settings();
+
                 wp_enqueue_script('ethers', 'https://cdnjs.cloudflare.com/ajax/libs/ethers/6.5.1/ethers.umd.min.js', array(), '6.5.1', true);
 
-                // Define the data to be passed to the JavaScript file
-                $sc_flow_plugin_settings = array(
-                    'pluginName' => MC_PLUGIN_NAME,
-                    // sc-flow-function_MAX_SUPPLY
-                    'maxSupply' => get_option(MC_ADMIN_FUNCTIONS_FIELDS_PREFIX . 'MAX_SUPPLY'),
-                    'mintPrice' => get_option(MC_ADMIN_FUNCTIONS_FIELDS_PREFIX . 'mintPrice'),
-                    'maxQuantity' => get_option(MC_ADMIN_FUNCTIONS_FIELDS_PREFIX . 'maxQuantity'),
-                    'totalSupply' => get_option(MC_ADMIN_FUNCTIONS_FIELDS_PREFIX . 'totalSupply'),
-                    'contractAddress' => get_option(MC_ADMIN_CONTRACT_ADDRESS_FIELD),
-                    'contractABI' => get_option(MC_ADMIN_CONTRACT_ABI_FIELD),
-                    'activeChain' => get_option(MC_ADMIN_ACTIVE_CHAIN_FIELD),
-                    // wallet connect
-                    'wcProjectId' => get_option(MC_ADMIN_WALLETCONNECT_FIELD),
-                    // Alchemy Provider
-                    'alchemyProvider' => get_option(MC_ADMIN_ALCHEMYPROVIDER_FIELD),
-                    // Fiat currency 
-                    'fiatCurrency' => get_option(MC_ADMIN_FIAT_CURRENCY_FIELD),
-                    // styles
-                    'minterCounterIdOrClass' => get_option(MC_PLUGIN_MINTER_COUNTER),
-                    'connectButtonIdOrClass' => get_option(MC_PLUGIN_CONNECT_BUTTON),
-                    'mintButtonIdOrClass' => get_option(MC_PLUGIN_MINT_BUTTON),
-                    'mintQuantityIdOrClass' => get_option(MC_PLUGIN_MINT_QUANTITY),
-                    // 
-                    'popup' => array(
-                        'errorQuantity' => __('Maximum quantity allowed is', 'sc-flow'),
-                        'errorChain' => __('Please switch to active chain', 'sc-flow'),
-                        'errorTermAndCondition' => __('Please accept the terms and conditions to buy NFT.', 'sc-flow'),
-                        'errorFunds' => __('Not enough funds in the wallet.', 'sc-flow'),
-                        'generalError' => __('An error occurred.', 'sc-flow'),
-                        'sucessMint' => __('Mint transaction is submitted successfully, you can view it', 'sc-flow'),
-                    )
-                );
 
                 // Enqueue non owner write smart contract operatons
                 wp_enqueue_script('sc-flow-helper', plugin_dir_url(PLUGIN_ROOT_PATH) . 'assets/js/sc-flow-helpers.js', array('jquery', 'ethers'), '0.0.1', true);
@@ -119,7 +94,7 @@ class SmartContract_Flow_Public
                 wp_enqueue_script('sc-flow-read', plugin_dir_url(PLUGIN_ROOT_PATH) . 'assets/js/sc-flow-read.js', array('jquery', 'ethers', 'sc-flow-helper'), '0.0.1', true);
 
                 // Enqueue js logic for mint ui component
-                wp_enqueue_script('sc-flow-frontend', plugin_dir_url(PLUGIN_ROOT_PATH) . 'assets/js/sc-flow-frontend.js', array('jquery', 'ethers', 'sc-flow-helper', 'sc-flow-read'), '0.0.1', true);
+                wp_enqueue_script('sc-flow-frontend', plugin_dir_url(PLUGIN_ROOT_PATH) . 'assets/js/sc-flow-frontend.js?v=0.0.3', array('jquery', 'ethers', 'sc-flow-helper', 'sc-flow-read'), '0.0.1', true);
 
                 // Enqueue crossmint script
                 wp_enqueue_script('crossmint', 'https://unpkg.com/@crossmint/client-sdk-vanilla-ui@1.0.1-alpha.6/lib/index.global.js', array('jquery', 'ethers', 'sc-flow-helper', 'sc-flow-read', 'sc-flow-frontend'), '0.1.0', true);
@@ -127,11 +102,51 @@ class SmartContract_Flow_Public
                 // Localize the script with the plugin settings
                 wp_localize_script('sc-flow-read', 'SCFlowPluginSettings', $sc_flow_plugin_settings);
                 wp_localize_script('sc-flow-frontend', 'SCFlowPluginSettings', $sc_flow_plugin_settings);
+                wp_localize_script('sc-flow-helper', 'SCFlowPluginSettings', $sc_flow_plugin_settings);
+
 
                 // Enqueue your custom styles
                 wp_enqueue_style('sc-flow-style', plugin_dir_url(PLUGIN_ROOT_PATH) . 'assets/css/sc-flow-frontend.css', array(), '0.0.1');
             }
         }
+
+    }
+
+    protected function get_sc_flow_plugin_settings()
+    {
+        // Define the data to be passed to the JavaScript file
+        $sc_flow_plugin_settings = array(
+            'pluginName' => MC_PLUGIN_NAME,
+            // sc-flow-function_MAX_SUPPLY
+            'maxSupply' => get_option(MC_ADMIN_FUNCTIONS_FIELDS_PREFIX . 'maxSupply'),
+            'mintPrice' => get_option(MC_ADMIN_FUNCTIONS_FIELDS_PREFIX . 'mintPrice'),
+            'maxQuantity' => get_option(MC_ADMIN_FUNCTIONS_FIELDS_PREFIX . 'maxQuantity'),
+            'totalSupply' => get_option(MC_ADMIN_FUNCTIONS_FIELDS_PREFIX . 'totalSupply'),
+            'contractAddress' => get_option(MC_ADMIN_CONTRACT_ADDRESS_FIELD),
+            'contractABI' => get_option(MC_ADMIN_CONTRACT_ABI_FIELD),
+            'activeChain' => get_option(MC_ADMIN_ACTIVE_CHAIN_FIELD),
+            // wallet connect
+            'wcProjectId' => get_option(MC_ADMIN_WALLETCONNECT_FIELD),
+            // Alchemy Provider
+            'alchemyProvider' => get_option(MC_ADMIN_ALCHEMYPROVIDER_FIELD),
+            // Fiat currency 
+            'fiatCurrency' => get_option(MC_ADMIN_FIAT_CURRENCY_FIELD),
+            // styles
+            'minterCounterIdOrClass' => get_option(MC_PLUGIN_MINTER_COUNTER),
+            'connectButtonIdOrClass' => get_option(MC_PLUGIN_CONNECT_BUTTON),
+            'mintButtonIdOrClass' => get_option(MC_PLUGIN_MINT_BUTTON),
+            'mintQuantityIdOrClass' => get_option(MC_PLUGIN_MINT_QUANTITY),
+            // 
+            'popup' => array(
+                'errorQuantity' => __('Maximum quantity allowed is', 'sc-flow'),
+                'errorChain' => __('Please switch to active chain', 'sc-flow'),
+                'errorTermAndCondition' => __('Please accept the terms and conditions to buy NFT.', 'sc-flow'),
+                'errorFunds' => __('Not enough funds in the wallet.', 'sc-flow'),
+                'generalError' => __('An error occurred.', 'sc-flow'),
+                'sucessMint' => __('Mint transaction is submitted successfully, you can view it', 'sc-flow'),
+            )
+        );
+        return $sc_flow_plugin_settings;
 
     }
     /**
