2831b8943e57bd6e1f92caca7ae01b9b863e6b03
Mohamed Habbat <mohamedhabbat@icloud.com>
2024-02-24 14:10:39 +0100

    fix: add try

--- assets/js/sc-flow-read.js
+++ assets/js/sc-flow-read.js
@@ -1,61 +1,81 @@
 // Read operation
 window.getMintPrice = async function getMintPrice(fn) {
   // Convert the price to wei (1 MATIC = 10^18 wei)
-  const data = await readContract({
-    address: SCFlowPluginSettings.contractAddress,
-    abi: SCFlowPluginSettings.contractABI,
-    functionName: "mintPrice",
-  });
-  const maticAmount = ethers.formatUnits(data, "ether");
-  fn(maticAmount);
+  try {
+    const data = await readContract({
+      address: SCFlowPluginSettings.contractAddress,
+      abi: SCFlowPluginSettings.contractABI,
+      functionName: "mintPrice",
+    });
+    const maticAmount = ethers.formatUnits(data, "ether");
+    fn(maticAmount);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 window.getMaxQuantity = async function getMaxQuantity(fn) {
   // Convert the price to wei (1 MATIC = 10^18 wei)
-  const data = await readContract({
-    address: SCFlowPluginSettings.contractAddress,
-    abi: SCFlowPluginSettings.contractABI,
-    functionName: "maxQuantity",
-  });
-  fn(data);
+  try {
+    const data = await readContract({
+      address: SCFlowPluginSettings.contractAddress,
+      abi: SCFlowPluginSettings.contractABI,
+      functionName: "maxQuantity",
+    });
+    fn(data);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 window.getTotalSupply = async function getTotalSupply(fn) {
   // Convert the price to wei (1 MATIC = 10^18 wei)
-  const data = await readContract({
-    address: SCFlowPluginSettings.contractAddress,
-    abi: SCFlowPluginSettings.contractABI,
-    functionName: "totalSupply",
-  });
-  fn(data);
+  try {
+    const data = await readContract({
+      address: SCFlowPluginSettings.contractAddress,
+      abi: SCFlowPluginSettings.contractABI,
+      functionName: "totalSupply",
+    });
+    fn(data);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 window.getMaxSupply = async function getMaxSupply(fn) {
   // Convert the price to wei (1 MATIC = 10^18 wei)
-  const data = await readContract({
-    address: SCFlowPluginSettings.contractAddress,
-    abi: SCFlowPluginSettings.contractABI,
-    functionName: "maxSupply",
-  });
-  fn(data);
+  try {
+    const data = await readContract({
+      address: SCFlowPluginSettings.contractAddress,
+      abi: SCFlowPluginSettings.contractABI,
+      functionName: "maxSupply",
+    });
+    fn(data);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 window.mint = async function mint(quantity, fn) {
-  // Convert the price to wei (1 MATIC = 10^18 wei)
-  const address = window.localStorage.getItem("WALLET_ADDRESS");
-  // Call the mint function
-  const value = ethers.parseUnits(
-    (quantity * Number.parseFloat(SCFlowPluginSettings.mintPrice)).toString(),
-    "ether"
-  );
-  const { request } = await prepareWriteContract({
-    address: SCFlowPluginSettings.contractAddress,
-    abi: SCFlowPluginSettings.contractABI,
-    functionName: "mintTo",
-    args: [quantity, address],
-    // gas: estimatedGas,
-    value: value,
-  });
-  const { hash } = await writeContract(request);
-  fn(hash);
+  try {
+    // Convert the price to wei (1 MATIC = 10^18 wei)
+    const address = window.localStorage.getItem("WALLET_ADDRESS");
+    // Call the mint function
+    const value = ethers.parseUnits(
+      (quantity * Number.parseFloat(SCFlowPluginSettings.mintPrice)).toString(),
+      "ether"
+    );
+    const { request } = await prepareWriteContract({
+      address: SCFlowPluginSettings.contractAddress,
+      abi: SCFlowPluginSettings.contractABI,
+      functionName: "mintTo",
+      args: [quantity, address],
+      // gas: estimatedGas,
+      value: value,
+    });
+    const { hash } = await writeContract(request);
+    fn(hash);
+  } catch (error) {
+    console.log(error);
+  }
 };
